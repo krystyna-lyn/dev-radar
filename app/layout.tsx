b1e2ee71410d2dc96cf0ba6c2cc27a6ac475c6ad
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2026-09-11 21:17:26 +0100

    create EventCard

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Schibsted_Grotesk, Martian_Mono, Geist } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import LightRays from "@/components/LightRays";
+import Navbar from "@/components/Navbar";
 
 const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -34,6 +35,8 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
           martianMono.variable,
         )}
       >
+        <Navbar />
+
         <div className="absolute inset-0 top-0 z-[-1] min-h-screen">
           <LightRays
             raysOrigin="top-center-offset"
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
+import ExploreBtn from "@/components/ExploreBtn"
+
 const Home = () => {
   return (
-    <div className="text-center text-2xl font-bold mt-10">
-      <h1>Discover developer events near you</h1>
-    </div>
+    <section>
+      <h1 className="text-center">Discover developer events near you</h1>
+      <p className="text-center mt-5">
+        Have you ever wanted to find developer events happening in your area?
+        Look no further! Dev Radar is here to help you discover and connect with the developer community around the world.
+        Hakathons, conferences, meetups, and more are just a click away</p>
+
+      <ExploreBtn />
+
+
+
+    </section>
   )
 }
 
