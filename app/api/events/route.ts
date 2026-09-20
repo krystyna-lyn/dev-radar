12043a139a3c238aed4755c24258c85cb0bd31d5
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2026-09-20 20:09:27 +0100

    add GET function

--- app/api/events/route.ts
+++ app/api/events/route.ts
@@ -47,4 +47,16 @@ export async function POST(req: NextRequest) {
     }
 }
 
+export async function GET() {
+    try {
+        await connectDB(); // Ensure the database connection is established
+        const events = await Event.find().sort({ createdAt: -1 });
+
+        return NextResponse.json({ message: "Events fetched successfully", events }, { status: 200 })
+    }
+    catch (e) {
+        return NextResponse.json({ message: "Error fetching events", error: e }, { status: 500 })
+    }
+}
+
 
