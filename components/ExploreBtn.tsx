a0eb00985e968ba86a985f4e62f5626e8a612123
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2026-09-21 20:59:13 +0100

    implement events from database

--- components/ExploreBtn.tsx
+++ components/ExploreBtn.tsx
@@ -1,8 +1,6 @@
 'use client'
 
 import Image from "next/image"
-import EventCard from "./EventCard"
-import { events } from "@/lib/constants/events"
 
 const ExploreBtn = () => {
     return (
@@ -13,17 +11,7 @@ const ExploreBtn = () => {
                     <Image src="/icons/arrow-down.svg" alt="arrow down" width={24} height={24} className="w-4 h-auto" />
                 </a>
             </button>
-            <div className="mt-20 space-y-7">
-                <h3>Featured Events</h3>
-                <ul className="events">
-                    {events.map((event) => (
-                        <li key={event.title}>
-                            <EventCard {...event} />
-                        </li>
-                    ))}
-                </ul>
 
-            </div>
         </section>
     )
 }
